495cf2c1c209de39ea376c6364d33eaf9787d8d5
paulalcabasa <paul-alcabasa@isuzuphil.com>
2018-02-15 11:10:52 +0800

    Update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ecommerce5/finance_bi/';
+//$config['base_url'] = 'http://ecommerce5/finance_bi/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/finance_bi/';
 
 /*
 |--------------------------------------------------------------------------
